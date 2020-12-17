b7fe272c4ce3911630900d6857af55cdda9bf0b5
MarianaES <mariana.enriquez.s@gmail.com>
2020-12-16 20:28:40 -0600

    Practice attribute directives

--- lesson3/examples/src/app/buttons/buttons.component.ts
+++ lesson3/examples/src/app/buttons/buttons.component.ts
@@ -7,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ButtonsComponent implements OnInit {
    buttonHeading: string = "Buttons"
-   inactive: boolean = false;
+   active: boolean = true;
+   buttonState: boolean[] = [true, true, true];
+   location: string = 'center';
 
    constructor() { }
 
    ngOnInit() { }
 
+   reactivateButtons() {
+      this.buttonState.forEach(button => {
+         let index: number = this.buttonState.indexOf(button);
+         
+         this.buttonState[index]= true;
+      })
+
+      return this.buttonState;
+   }
+
+   shiftLocation(oldLocation: string) {
+      let positions: string[] = ['right', 'left', 'center'];
+
+      while (this.location === oldLocation) {
+         let randomIndex: number = Math.floor(Math.random()*positions.length);
+         this.location = positions[randomIndex];
+      }
+
+      return this.location;
+
+   }
+   
 }
